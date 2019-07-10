99593e64f5023b667400cfc4f3e1c5f8840ae737
Indunil1992 <indunil@adroitlogic.com>
2019-07-10 09:51:09 +0530

    lo

--- indunilcaliforn/lambda.js
+++ indunilcaliforn/lambda.js
@@ -1,18 +1,37 @@
+let AWS = require('aws-sdk');
 let SL_AWS = require('slappforge-sdk-aws');
-let connectionManager = require('./ConnectionManager');
-const rds = new SL_AWS.RDS(connectionManager);
+const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
 
+    sqs.receiveMessage({
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/new`,
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '10',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '0'
+    }).promise()
+        .then(receivedMsgData => {
+            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
+                let receivedMessages = receivedMsgData.Messages;
+                receivedMessages.forEach(message => {
+                    // your logic to access each message through out the loop. Each message is available under variable message 
+                    // within this block
+                    console.log("msg with receive");
+                 console.log( message );
+                });
+            } else {
+                console.log("Success");
+                 //console.log( data );
+                // No messages to process
+            }
+        })
+        .catch(err => {
+            console.log("not Success");
+                 console.log( err );
+            // error handling goes here
+        });
 
-    // You must always end/destroy the DB connection after it's used
-    rds.beginTransaction({
-        instanceIdentifier: 'indunil'
-    }, function (error, connection) {
-        if (error) {
-            throw error;
-        }
-    });
 
 
 
